4ced8a35b68468a2b8359f94c5b35b2142d567b4
heling <helingchn@foxmail.com>
2020-12-04 19:00:54 +0800

    添加LeetCode122#买股票最佳时机-动态规划解法

--- src/main/java/org/example/leetcode/solutions/Solution122.java
+++ src/main/java/org/example/leetcode/solutions/Solution122.java
@@ -4,18 +4,35 @@ package org.example.leetcode.solutions;
  *
  **/
 public class Solution122 {
-    public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            maxProfit += Math.max(0, prices[i] - prices[i - 1]);
+    static class SolutionGreedy {
+        public int maxProfit(int[] prices) {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.length; i++) {
+                maxProfit += Math.max(0, prices[i] - prices[i - 1]);
+            }
+            return maxProfit;
         }
-        return maxProfit;
     }
 
+    static class SolutionDynamicProgram {
+        public int maxProfit(int[] prices) {
+            int[][] dp = new int[prices.length][2];
+
+            dp[0][0] = 0;
+            dp[0][1] = -prices[0];
+            for (int i = 1; i < prices.length; i++) {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            }
+            return dp[prices.length - 1][0];
+        }
+    }
+
+
     public static void main(String[] args) {
-        Solution122 solution122 = new Solution122();
-        int[] prices = {1, 2, 1, -1, 0, -3};
-        int i = solution122.maxProfit(prices);
+        SolutionDynamicProgram solution = new SolutionDynamicProgram();
+        int[] prices = {7,1,5,3,6,4};
+        int i = solution.maxProfit(prices);
         System.out.println(i);
     }
 }
