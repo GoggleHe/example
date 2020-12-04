62ce8c19a696f33ecaee8c9704b3341a999ed298
heling <helingchn@foxmail.com>
2020-12-04 14:59:51 +0800

    添加LeetCode122#买股票最佳时机-贪心解法

--- src/main/java/org/example/leetcode/solutions/Solution122.java
+++ src/main/java/org/example/leetcode/solutions/Solution122.java
@@ -5,9 +5,17 @@ package org.example.leetcode.solutions;
  **/
 public class Solution122 {
     public int maxProfit(int[] prices) {
-        for (int i = 0; i < prices.length; i++) {
-
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit += Math.max(0, prices[i] - prices[i - 1]);
         }
-        return 0;
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Solution122 solution122 = new Solution122();
+        int[] prices = {1, 2, 1, -1, 0, -3};
+        int i = solution122.maxProfit(prices);
+        System.out.println(i);
     }
 }
