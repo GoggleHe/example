164365ead0a58d62e17947e028d9c3a943a58357
heling <helingchn@foxmail.com>
2020-12-11 16:30:38 +0800

    添加LeetCode21#合并两个有序列表-已解决

--- example/src/main/java/org/example/leetcode/solutions/Solution21.java
+++ example/src/main/java/org/example/leetcode/solutions/Solution21.java
@@ -16,38 +16,27 @@ package org.example.leetcode.solutions;
  **/
 public class Solution21 {
     public static void main(String[] args) {
-
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3)));
+        ListNode l2 = new ListNode(2, new ListNode(5, new ListNode(7)));
+        Solution21 solution = new Solution21();
+        ListNode listNode = solution.mergeTwoLists(l1, l2);
+        System.out.println(listNode);
     }
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode res = null;
-        ListNode node = null;
-        while(l1 != null && l2 != null){
-            ListNode temp = null;
-            if(l1.val <= l2.val){
-                node = new ListNode(l1.val);
+        ListNode head = new ListNode(-1);
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
                 l1 = l1.next;
-            }else{
-                node = new ListNode(l2.val);
+            } else {
+                cur.next = l2;
                 l2 = l2.next;
             }
-            if(res == null && temp != null){
-                res = temp;
-                node = res;
-            } else {
-                node.next = temp;
-                node = node.next;
-            }
-
+            cur = cur.next;
         }
-        while(l1 != null){
-            node = new ListNode(l1.val);
-            l1 = l1.next;
-        }
-        while(l2 != null){
-            node = new ListNode(l2.val);
-            l2 = l2.next;
-        }
-        return res;
+        cur.next = l1 != null ? l1 : l2;
+        return head.next;
     }
 }
 
