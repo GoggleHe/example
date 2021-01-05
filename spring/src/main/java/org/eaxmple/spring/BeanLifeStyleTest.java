01f650bea090f0950d80a030b3d54713aaade1a5
heling <helingchn@foxmail.com>
2021-01-05 16:17:34 +0800

    添加Spring生命周期示例

---
+++ spring/src/main/java/org/eaxmple/spring/BeanLifeStyleTest.java
@@ -0,0 +1,18 @@
+package org.eaxmple.spring;
+
+import org.eaxmple.spring.beans.SpringBoy;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ **/
+public class BeanLifeStyleTest {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-boy-spring.xml");
+        SpringBoy hello = (SpringBoy) context.getBean("SpringBoy");
+//        System.out.println(hello);
+        context.registerShutdownHook();
+    }
+
+}
---
+++ spring/src/main/java/org/eaxmple/spring/beans/SpringBoy.java
@@ -0,0 +1,78 @@
+package org.eaxmple.spring.beans;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.EmbeddedValueResolverAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.StringValueResolver;
+
+/**
+ *
+ **/
+public class SpringBoy implements BeanNameAware, BeanClassLoaderAware, ResourceLoaderAware, EmbeddedValueResolverAware, EnvironmentAware, BeanFactoryAware, InitializingBean, DisposableBean {
+    private String beanName;
+    private String name;
+
+    public SpringBoy() {
+        System.out.println("constructor");
+        System.out.println("name = " + name);
+    }
+
+    private void init() {
+        System.out.println("init");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactoryAware.setBeanFactory");
+    }
+
+    public void setName(String name) {
+        System.out.println("setName");
+        this.name = "name";
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("BeanNameAware.setBeanName");
+        this.beanName = name;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean.destroy");
+    }
+
+    private void customDestroy() {
+        System.out.println("customDestroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean.afterPropertiesSet");
+        System.out.println("name = " + name);
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("BeanClassLoaderAware.setBeanClassLoader");
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        System.out.println("EnvironmentAware.setEnvironment");
+    }
+
+    @Override
+    public void setEmbeddedValueResolver(StringValueResolver resolver) {
+        System.out.println("EmbeddedValueResolverAware.setEmbeddedValueResolver");
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        System.out.println("ResourceLoaderAware.setResourceLoader");
+    }
+}
