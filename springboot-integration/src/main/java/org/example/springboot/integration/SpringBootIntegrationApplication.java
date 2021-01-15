07365c7415554f8e6ec9341ec80076a58a71067b
heling <helingchn@foxmail.com>
2021-01-15 19:10:34 +0800

    添加springboot集成示例

---
+++ springboot-integration/src/main/java/org/example/springboot/integration/SpringBootIntegrationApplication.java
@@ -0,0 +1,14 @@
+package org.example.springboot.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ **/
+@SpringBootApplication
+public class SpringBootIntegrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntegrationApplication.class, args);
+    }
+}
