67c3616564b70a4531fa39a1b5c9f7a4667b88e7
heling <helingchn@foxmail.com>
2021-02-01 18:00:08 +0800

    添加mybatis批量更新示例

--- springboot-integration/src/main/java/org/example/springboot/integration/mybatis/mapper/MyMapper.java
+++ springboot-integration/src/main/java/org/example/springboot/integration/mybatis/mapper/MyMapper.java
@@ -1,13 +1,21 @@
 package org.example.springboot.integration.mybatis.mapper;
 
+import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@CacheNamespace()
 public interface MyMapper {
 
     @Select("select USE_ID from USER")
     List<Integer> selectOne();
+
+    @Update("update user set use_name = #{name1} where use_id = #{id1};" +
+            "update user set use_name = #{name2} where use_id = #{id2};")
+    int updateMulti(@Param("id1") Integer id1, @Param("name1") String name1, @Param("id2") Integer id2, @Param("name2") String name2);
 }
